0d28516ebd539e02a14574e88855edabc21a809c
iTaxEvade <124311420+iTaxEvade@users.noreply.github.com>
2025-02-13 21:14:42 -0800

    Polygon Party

--- Assets/Scripts/SizeChange2.cs
+++ Assets/Scripts/SizeChange2.cs
@@ -13,12 +13,12 @@ public class SizeChange2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Keypad2) && transform.localScale.x < 4f && transform.localScale.y < 4f)
+        if (Input.GetKey(KeyCode.Slash) && transform.localScale.x < 4f && transform.localScale.y < 4f)
         {
             transform.localScale = new Vector3(transform.localScale.x + 0.01f, transform.localScale.y + 0.01f, 1);
         }
 
-        if (Input.GetKey(KeyCode.Keypad1) && transform.localScale.x > 0.4f && transform.localScale.y > 0.4f)
+        if (Input.GetKey(KeyCode.Period) && transform.localScale.x > 0.4f && transform.localScale.y > 0.4f)
         {
             transform.localScale = new Vector3(transform.localScale.x - 0.01f, transform.localScale.y - 0.01f, 1);
         }
